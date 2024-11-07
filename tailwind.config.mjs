cbd9cae191f2dea2468d453b8991da8ceedf199b
Boris Grunwald <borisgrunwald@gmail.com>
2024-11-07 10:25:21 +0100

    wip

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+	  extend: {
+		colors: {
+		  main: "#182C72",
+		  secondary: "#0C216B",
+		  tertiary: "#37DEAD",
+		  dark: "#011A51",
+		  white: "#fff",
+		  black: "#000"
+		},
+		fontFamily: {
+		  poppins: ["Poppins", "sans-serif"]
+		},
+		typography: (theme) => ({
+		  DEFAULT: {
+			css: {
+			  fontFamily: theme('fontFamily.poppins').join(', '),
+			},
+		  },
+		})
+	  }
+	},
+	plugins: [
+	  require('@tailwindcss/typography')
+	]
+  };
+  
