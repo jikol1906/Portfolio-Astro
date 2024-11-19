a966130a2c5b71a1aba2b914c0f252e0c3982ad8
Boris Grunwald <borisgrunwald@gmail.com>
2024-11-19 11:40:58 +0100

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,32 @@
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-	  extend: {
-		colors: {
-		  main: "#182C72",
-		  secondary: "#0C216B",
-		  tertiary: "#37DEAD",
-		  dark: "#011A51",
-		  white: "#fff",
-		  black: "#000"
-		},
-		backgroundImage: {
-			'card-bg': "url('/img/card-bg.svg')",
-		},
-		fontFamily: {
-		  poppins: ["Poppins", "sans-serif"]
-		},
-		typography: (theme) => ({
-		  DEFAULT: {
-			css: {
-			  fontFamily: theme('fontFamily.poppins').join(', '),
-			},
-		  },
-		})
-	  }
-	},
-	plugins: [
-	  require('@tailwindcss/typography')
-	]
-  };
-  
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#182C72",
+        secondary: "#0C216B",
+        tertiary: "#37DEAD",
+        dark: "#011A51",
+        white: "#fff",
+        black: "#000"
+      },
+      borderRadius: {
+        "custom-xl": "0 1rem 0 1rem" // Customizing the corners (tr, tl, br, bl).
+      },
+      backgroundImage: {
+        "card-bg": "url('/img/card-bg.svg')"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            fontFamily: theme("fontFamily.poppins").join(", ")
+          }
+        }
+      })
+    }
+  },
+  plugins: [require("@tailwindcss/typography")]
+};
